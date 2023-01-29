9405c0aeca85998a7d72131f49bb8d2e7096e306
Anton Kosyakov <anton@gitpod.io>
2023-01-29 21:30:31 +0000

    init

---
+++ .gitpod.Dockerfile
@@ -0,0 +1,3 @@
+FROM gitpod/workspace-base:latest
+
+COPY /etc/init.d/docker-compose /etc/init.d/docker-compose
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-trap 'docker-compose down' TERM INT
-docker-compose up &
-PID=$!
-wait $PID
-trap - TERM INT
-wait $PID
-EXIT_STATUS=$?
